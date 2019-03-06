b9a54a8c6ae3644bdbb5fcbcc247b2a40d86d9ab
vvcb <vvcb.n1@gmail.com>
2019-03-06 23:26:38 +0000

    PWA Test
    Add baseurl to all assets (except images in posts)

---
+++ sw.js
@@ -0,0 +1,4 @@
+---
+---
+const allUrls = {%- include functions/manifest.liquid -%}
+{% include shell/js/_sw.js %}
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "_site/",
+  "globPatterns": [
+    "**/*.{html,css,json,jpg,jpeg,png,js}"
+  ],
+  "swDest": "sw.js",
+  "swSrc": "_src/sw.js"
+};
