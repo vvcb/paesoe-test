8b71a6525641845768a536e543a8f2a1b016357d
vvcb <deploy@travis-ci.org>
2019-03-10 16:37:06 +0000

    Deploy paesoe.org to github.com/vvcb/paesoe-test.git:gh-pages

---
+++ workbox-config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  globDirectory: "_site/",
+  globPatterns: [
+    "**/*.{html,css,json,jpg,jpeg,png,webp,js}"
+  ],
+  swDest: "./sw.js",
+  // manifestTransforms: [
+  //   // Append baseurl to paths
+  //   (originalManifest) => {
+  //     const BASEURL = 'paesoe-test/';
+  //     const manifest = originalManifest.map(entry => {
+  //       if (!(entry.url.startsWith('http') ||
+  //            entry.url.startsWith('www'))){
+  //         entry.url = BASEURL + entry.url;
+  //       }
+  //       return entry;
+  //     });
+      
+  //     return {manifest, warnings: []};
+  //   }
+  // ],
+  offlineGoogleAnalytics: {
+    cacheName: 'paesoe-ga-cache',
+    parameterOverrides: {
+      cd1: 'offline'
+    },
+    hitFilter: (params) => {
+      const queueTimeInSeconds = Math.round(params.get('qt') / 1000);
+      params.set('cm1', queueTimeInSeconds);
+    }
+  },
+  // cleanupOutdatedCaches: true,
+  runtimeCaching: [{
+      urlPattern: new RegExp('data\\.+\.json$'),
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'paesoe-data-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30
+        },
+        backgroundSync: {
+          name: 'paesoeDataQueue',
+          options: {
+            maxRetentionTime: 24 * 60,
+          }
+        }
+      }
+    },
+    {
+      urlPattern: RegExp('\.(?:png|jpg|jpeg|webp|gif|svg)$'),
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'paesoe-image-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30
+        },
+        backgroundSync: {
+          name: 'paesoeImageQueue',
+          options: {
+            maxRetentionTime: 24 * 60,
+          }
+        }
+      }
+    }
+  ]
+};
